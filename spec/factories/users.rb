1ba8eb6dbb35fe64832bca36b9093ffbedbd374b
yuta.saito <seifuzi2064@gmail.com>
2023-12-10 08:16:33 +0900

    テスト時に見やすくするため、FactoryBotのuserのnameが生成毎に変わるように変更

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@
 
 FactoryBot.define do
   factory :user do
-    name { 'factory_user' }
+    sequence(:name) { |n| "'factory_tester_#{n}" }
     sequence(:email) { |n| "'factory_tester#{n}@email.com" }
     password { 'factory_pw' }
   end

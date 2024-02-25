47a52aa4a3f32a3094178009674134a36034b327
Yuta Saito <seifuzi2064@gmail.com>
2024-02-25 08:49:51 +0000

    モーダルの詳細画面で次へボタンと前へボタンを追加

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -1,21 +1,58 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
+  static targets = ["detailButton"];
+
+  connect() {
+    this.shoppingIds = this.detailButtonTargets.map(
+      (button) => button.dataset.shoppingId
+    );
+    this.currentShoppingId = null;
+  }
+
   // hiddenになっていたモーダル要素を表示する
-  async showModal(evt) {
+  showModal(evt) {
+    this.currentShoppingId = evt.target.dataset.shoppingId;
     this.toggleModal(true);
-    await this.makeModalContent(evt, modal);
+    this.makeModalContent();
   }
 
   // ×ボタンを押したときにモーダルとオーバーレイを閉じるメソッド
-  closeModal(evt) {
+  closeModal() {
     this.toggleModal(false);
   }
 
+  prevPage(evt) {
+    this.switchPage("prev");
+  }
+
+  nextPage(evt) {
+    this.switchPage("next");
+  }
+
+  // 前ページに行くか、次ページに行くか決めるメソッド
+  switchPage(direction) {
+    const currentIndex = this.shoppingIds.indexOf(this.currentShoppingId);
+    let newIndex = 0;
+    if (direction === "next") {
+      // 最後のshoppingを表示していた時に、次へボタンを押したら最初の要素を表示する
+      // 例: [1,3,5]の配列があって、現在5の要素を表示していた。その時にnextPageを呼び出すと、次は1の要素を表示する
+      newIndex =
+        currentIndex === this.shoppingIds.length - 1 ? 0 : currentIndex + 1;
+    } else if (direction === "prev") {
+      // 最初のshoppingを表示していた時に、前へボタンを押したら最後の要素を表示する
+      // 例: [1,3,5]の配列があって、現在1の要素を表示していた。その時にprevPageを呼び出すと、次は5の要素を表示する
+      newIndex =
+        currentIndex === 0 ? this.shoppingIds.length - 1 : currentIndex - 1;
+    }
+    this.currentShoppingId = this.shoppingIds[newIndex];
+    this.switchOldToNewModal();
+  }
+
   // モーダルの内容を作成
-  async makeModalContent(evt, modal) {
+  async makeModalContent() {
     // 詳細ボタンを押下した要素のデータをAPIで取得
-    const shoppingId = evt.target.dataset.shoppingId;
+    const shoppingId = this.currentShoppingId;
     const data = await this.fetchData(`shoppings/${shoppingId}/modal`);
 
     // 買物名を表示
@@ -89,14 +126,20 @@ export default class extends Controller {
   toggleModal(isVisible) {
     const modal = document.getElementById("modal");
     const overlay = document.getElementById("overlay");
+    const prevBtn = document.getElementById("prev-button");
+    const nextBtn = document.getElementById("next-button");
     const detailsContentNode = document.getElementById("details-content");
 
     if (isVisible) {
       modal.classList.remove("hidden");
       overlay.classList.remove("hidden");
+      prevBtn.classList.remove("hidden");
+      nextBtn.classList.remove("hidden");
     } else {
       modal.classList.add("hidden");
       overlay.classList.add("hidden");
+      prevBtn.classList.add("hidden");
+      nextBtn.classList.add("hidden");
       this.removeAllChildNodes(detailsContentNode);
     }
   }
@@ -106,5 +149,12 @@ export default class extends Controller {
     while (node.firstChild) {
       node.removeChild(node.firstChild);
     }
+  }
+
+  // ページ移動の際に、前回表示していたモーダルを削除してから次のモーダルを表示するメソッド
+  switchOldToNewModal() {
+    this.toggleModal(false);
+    this.toggleModal(true);
+    this.makeModalContent();
   }
 }
